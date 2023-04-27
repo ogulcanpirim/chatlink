5ae4843d9ac768dbc0a657330355dd5a7bb5a031
Oğulcan Pirim <ogulcan.pirim@upayments.com>
2023-04-28 02:23:46 +0300

    update: modal transitions

--- client/src/components/FriendsModal.tsx
+++ client/src/components/FriendsModal.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppSelector } from "../hooks/useAppSelector";
 import { useDispatch } from "react-redux";
 import { setFriendModal } from "../store/reducers/pageReducer";
 import PendingFriendCard from "./PendingFriendCard";
-import { Portal } from "@headlessui/react";
+import { Portal, Transition } from "@headlessui/react";
 
 const FriendsModal = () => {
   const { friendModal, darkMode } = useAppSelector((state) => state.page);
-
+  const [show, setShow] = useState(true);
   const dispatch = useDispatch();
 
   const [pendingRequest, setPendingRequest] = React.useState<any>([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
   ]);
 
   if (!friendModal) {
@@ -23,23 +23,38 @@ const FriendsModal = () => {
       <div
         onClick={(e) => {
           if (e.target === e.currentTarget) {
-            dispatch(setFriendModal(false));
+            setShow(false);
           }
         }}
         className={`${
           darkMode && "dark"
         } fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center`}
       >
-        <div className="bg-white dark:bg-gray-800 rounded-lg p-4 w-96">
-          <div className="text-black dark:text-white text-lg text-center font-semibold py-4 border-b-[0.1rem] border-b-gray-200 dark:border-b-gray-700">
-            Pending Request
+        <Transition
+          show={show}
+          appear={true}
+          enter="transition-all ease-in-out duration-300 transform"
+          enterFrom="opacity-0 scale-50"
+          enterTo="opacity-100 scale-100"
+          leave="transition-all ease-in-out duration-300 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-50"
+          afterLeave={() => {
+            dispatch(setFriendModal(false));
+            setShow(true);
+          }}
+        >
+          <div className="bg-white dark:bg-gray-800 rounded-lg p-4 w-96">
+            <div className="text-black dark:text-white text-lg text-center font-semibold py-4 border-b-[0.1rem] border-b-gray-200 dark:border-b-gray-700">
+              Pending Request
+            </div>
+            <div className="flex flex-col h-96 overflow-y-auto py-2">
+              {pendingRequest.map((item: number) => (
+                <PendingFriendCard key={item.toString()} />
+              ))}
+            </div>
           </div>
-          <div className="flex flex-col h-96 overflow-y-auto py-2">
-            {pendingRequest.map((item: number) => (
-              <PendingFriendCard key={item.toString()} />
-            ))}
-          </div>
-        </div>
+        </Transition>
       </div>
     </Portal>
   );
--- client/src/components/PendingFriendCard.tsx
+++ client/src/components/PendingFriendCard.tsx
@@ -1,53 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import checkLogo from "../assets/check.svg";
 import cross from "../assets/cross.svg";
+import { Transition } from "@headlessui/react";
 
 const PendingFriendCard = () => {
+  const [show, setShow] = useState(true);
+
+  const height = document.getElementById("friendCard")?.clientHeight;
+
   return (
-    <div className="flex items-center justify-between py-2 px-4 border-b-[0.1rem] border-b-gray-200 dark:border-b-gray-700">
-      <div className="flex items-center">
-        <img
-          className="w-12 h-12 rounded-full"
-          src={`https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/f2/f2c80166a0aafda50418f306720ad1b7a9086759_full.jpg`}
-          alt="User Avatar"
-        />
-        <div className="ml-4">
-          <div className="text-black dark:text-white text-md font-semibold">
-            {"Oğulcan Pirim"}
-          </div>
-          <div className="text-gray-500 dark:text-gray-400 text-sm">
-            {"opirim@gmail.com"}
+    <Transition
+      show={show}
+      leave="transition-all ease-in-out duration-300 transform"
+      leaveFrom={`opacity-100 h-[${height}px] scale-100`}
+      leaveTo="opacity-0 h-0 scale-75"
+    >
+      <div
+        id="friendCard"
+        className="flex items-center justify-between py-2 px-4 border-b-[0.1rem] border-b-gray-200 dark:border-b-gray-700"
+      >
+        <div className="flex items-center">
+          <img
+            className="w-12 h-12 rounded-full"
+            src={`https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/f2/f2c80166a0aafda50418f306720ad1b7a9086759_full.jpg`}
+            alt="User Avatar"
+          />
+          <div className="ml-4">
+            <div className="text-black dark:text-white text-md font-semibold">
+              {"Oğulcan Pirim"}
+            </div>
+            <div className="text-gray-500 dark:text-gray-400 text-sm">
+              {"opirim@gmail.com"}
+            </div>
           </div>
         </div>
+        <div className="flex items-center">
+          <button
+            className="mr-4"
+            onClick={(e) => {
+              e.preventDefault();
+              show && setShow(false);
+            }}
+          >
+            <img
+              className="w-8 h-8 hover:bg-green-500 hover:rounded-full hover:bg-opacity-30"
+              src={checkLogo}
+              alt="Check Logo"
+            />
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("decline");
+              show && setShow(false);
+            }}
+          >
+            <img
+              className="w-8 h-8 border-red-500 hover:bg-red-500 hover:rounded-full hover:bg-opacity-30"
+              src={cross}
+              alt="Cross Logo"
+            />
+          </button>
+        </div>
       </div>
-      <div className="flex items-center">
-        <button
-          className="mr-4"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log("accept");
-          }}
-        >
-          <img
-            className="w-8 h-8 hover:bg-green-500 hover:rounded-full hover:bg-opacity-30"
-            src={checkLogo}
-            alt="Check Logo"
-          />
-        </button>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            console.log("decline");
-          }}
-        >
-          <img
-            className="w-8 h-8 border-red-500 hover:bg-red-500 hover:rounded-full hover:bg-opacity-30"
-            src={cross}
-            alt="Cross Logo"
-          />
-        </button>
-      </div>
-    </div>
+    </Transition>
   );
 };
 
